3a1b4cc0ecfe0988890ba8a0514af98b4738ac6b
Jeff Hevrin <jhevrin2@gmail.com>
2021-02-24 13:59:11 -0600

    Added stuff for pypi

--- README.md
+++ README.md
@@ -8,3 +8,15 @@ https://docs.python.org/3/distutils/setupscript.html
 ```bash
 pip install git+https://github.com/jhevrin2/test-package.git
 ```
+
+### How to get your package to PyPI
+
+Good link to help with building your own PyPI package:  https://packaging.python.org/tutorials/packaging-projects/
+
+```bash
+pip install --upgrade build
+pip install --upgrade twine
+python -m build
+twine upload --repository testpypi dist/*
+pip install --index-url https://test.pypi.org/simple/ --no-deps jhevrin2_dostuff
+```
--- setup.py
+++ setup.py
@@ -1,10 +1,25 @@
-from distutils.core import setup
+import setuptools
 
-setup(
-    name='dostuff',
-    version='1.0',
-    description='Testing package for writing pip installable packages',
-    author='Jeff Hevrin',
-    author_email='jhevrin2@gmail.com',
-    packages=['dostuff']
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="jhevrin2-dostuff",
+    version="0.0.1",
+    author="Jeff Hevrin",
+    author_email="jhevrin2@gmail.com",
+    description="Test Pip Package",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/jhevrin2/test-package",
+    project_urls={
+        "Bug Tracker": "https://github.com/jhevrin2/test-package/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    packages=setuptools.find_packages(),
+    python_requires='>=3.6',
 )
